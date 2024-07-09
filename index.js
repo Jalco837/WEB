17716cd6a3eabdbdc6ca96438202c2485ae074a8
johan <johan_lc@tesch.edu.mx>
2024-07-09 14:07:48 -0600

    cambio

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send('Hola ya pude deployar')
+    res.send('Hola ya pude deployar es el comienzo')
 })
 app.listen(3000)
 console.log('Servidor en el puerto', 3000);
