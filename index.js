0a4a546e709a492b5e7583d7fceadc1f995a3557
johan <johan_lc@tesch.edu.mx>
2024-07-11 10:36:55 -0600

    cambios30

--- index.js
+++ index.js
@@ -50,48 +50,6 @@ app.get('/api/hotels', async (req, res) => {
   }
 });
 
-// Ruta para manejar la solicitud POST y guardar un hotel
-app.post('/api/hotels', async (req, res) => {
-  try {
-    const hotel = new Hotel(req.body);
-    await hotel.save();
-    res.status(201).send({ message: 'Hotel guardado exitosamente' });
-  } catch (error) {
-    res.status(500).send({ message: 'Error al guardar el hotel', error });
-  }
-});
-
-// Ruta para actualizar un hotel específico
-app.put('/api/hotels/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name, description, stars, images } = req.body;
-
-  try {
-    const hotel = await Hotel.findByIdAndUpdate(id, {
-      name,
-      description,
-      stars,
-      images
-    }, { new: true });
-
-    res.status(200).send({ message: 'Hotel actualizado exitosamente', hotel });
-  } catch (error) {
-    console.error('Error al actualizar el hotel:', error);
-    res.status(500).send({ message: 'Error interno al actualizar el hotel', error });
-  }
-});
-
-// Ruta para eliminar un hotel
-app.delete('/api/hotels/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-    await Hotel.findByIdAndDelete(id);
-    res.status(200).send({ message: 'Hotel eliminado exitosamente' });
-  } catch (error) {
-    res.status(500).send({ message: 'Error al eliminar el hotel', error });
-  }
-});
-
 // Servir archivo HTML principal (index.html)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
